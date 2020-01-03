487ff5ffa3802d07f543adff029c334d68f80d5c
SamT <example@example.com>
2020-01-03 15:51:08 +0000

    GH-4 added checking for duplicate character entry

--- hangman.py
+++ hangman.py
@@ -44,11 +44,15 @@ def draw_word(secret_word, length, replace, indexes, replacement):
     return secret_word
 
 
-def check_word(char, word):
+def check_word(char, word, secret_word):
     string_to_check = list(word)
     if char in string_to_check:
-        index = [i for i, x in enumerate(string_to_check) if x == char]
-        return index
+        if char not in secret_word:
+            index = [i for i, x in enumerate(string_to_check) if x == char]
+            return index
+        else:
+            print("you already chose this letter")
+            return []
     else:
         return "wrong"
 
@@ -76,7 +80,7 @@ def game():
             break
         else:
             char = input("\n")
-            index = check_word(char, word)
+            index = check_word(char, word, secret)
             if index == "wrong":
                 lives -= 1
                 new_secret = secret

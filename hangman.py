791165d06708d5c7a0572112016334926a94af80
SamT <example@example.com>
2020-01-03 13:14:45 +0000

    GH-2 added a list of 20 words

--- hangman.py
+++ hangman.py
@@ -1,5 +1,30 @@
 # basic hangman implimentation in python
 
+from random import choice
+
+words = [
+    "adventure",
+    "dependence",
+    "heel",
+    "collapse",
+    "establish",
+    "decline",
+    "press",
+    "glass",
+    "unpleasant",
+    "cafe",
+    "shareholder",
+    "therapist",
+    "withdraw",
+    "earthquake",
+    "institution",
+    "broadcast",
+    "tradition",
+    "perform",
+    "exile",
+    "economic"
+]
+
 name = input("What is you name?\n> ")
 
 print("Hello %s!" % name)
